8645fa71c9f8056d5af62a1e5f400db08e96fbd8
Jeff Fessler <fessler@umich.edu>
2023-01-29 19:39:10 -0500

    resnet

--- docs/lit/examples/04-denoise-1d.jl
+++ docs/lit/examples/04-denoise-1d.jl
@@ -1,11 +1,11 @@
 #=
 # [04-denoise-1d](@id 04-denoise-1d)
 
-## Basic Introduction to Machine Learning: 04-denoise-1d  
+## Basic Introduction to Machine Learning: 04-denoise-1d
 
 Illustrate 1D signal denoising using Julia's Flux library
 
-- Jeff Fessler, University of Michigan  
+- Jeff Fessler, University of Michigan
 - 2018-10-23 Julia 1.0.1 version
 - 2023-01-29 Julia 1.8.5 version
 
@@ -28,19 +28,17 @@ This page was generated from a single Julia file:
 
 # Packages needed here.
 
-using LinearAlgebra: norm
+using LinearAlgebra: norm, I
 using Random: seed!
-using Distributions: Normal
+using Distributions: Normal, randperm
 import Flux # Julia package for deep learning
-using Flux: throttle, mse
+using Flux: Dense, Conv, Chain, SkipConnection, Adam, mse, relu, SamePad
 using LaTeXStrings # pretty plot labels
-using Plots: plot, plot!, scatter, scatter!, histogram, default, font, gui
+using Plots: plot, plot!, scatter, scatter!, histogram, histogram2d, default, font, gui
 using MIRTjim: jim, prompt
 using InteractiveUtils: versioninfo
 
 default(markersize=5, markerstrokecolor=:auto, label="")
-#fnt = font("DejaVu Sans", 15) # larger default font
-#default(guidefont=fnt, xtickfont=fnt, ytickfont=fnt, legendfont=fnt)
 default(tickfontsize=10, legendfontsize=11)
 
 
@@ -56,7 +54,7 @@ Generate training and testing data; 1D piece-wise constant signals
 =#
 
 # Function to generate a random piece-wise constant signal
-function makestep(; dim=32, njump=3, valueDist=Normal(0,1), minsep=2)
+function makestep(; dim=32, njump=3, valueDist=Normal(0, 1), minsep=2)
     jump_locations = randperm(dim)[1:njump]
     while minimum(diff(jump_locations)) <= minsep
         jump_locations = randperm(dim)[1:njump] # random jump locations
@@ -65,8 +63,8 @@ function makestep(; dim=32, njump=3, valueDist=Normal(0,1), minsep=2)
     index[jump_locations] .= 1
     index = cumsum(index)
     values = rand(valueDist, njump) # random signal values
-    x = zeros(dim)
-    for jj=1:njump
+    x = zeros(Float32, dim)
+    for jj in 1:njump
         x[index .== jj] .= values[jj]
     end
     x[index .== 0] .= values[njump] # periodic end conditions
@@ -93,19 +91,19 @@ prompt()
 
 
 # Data covariance
-Kx = Xtrain*Xtrain' / ntrain
-p1 = jim(Kx, title="Kx");
+Kx = Xtrain * Xtrain' / ntrain
+p1 = jim(Kx, title="Kx", color=:cividis);
 
 # Add noise
 σnoise = 0.3
-Ytrain = Xtrain + σnoise * randn(size(Xtrain)) # noisy train data
-Ytest = Xtest + σnoise * randn(size(Xtest)) # noisy test data
-Ky = Ytrain*Ytrain' / ntrain;
+Ytrain = Xtrain + σnoise * randn(Float32, size(Xtrain)) # noisy train data
+Ytest = Xtest + σnoise * randn(Float32, size(Xtest)) # noisy test data
+Ky = Ytrain * Ytrain' / ntrain;
 
 @show maximum(Kx)
 @show maximum(Ky)
 
-p2 = jim(Ky, title="Ky")
+p2 = jim(Ky, title="Ky", color=:cividis)
 plot(p0, p1, p2, layout=(3,1))
 
 #
@@ -118,15 +116,20 @@ Wiener filter (MMSE estimator)
 
 ## cond(Kx + σnoise*I)
 Hw = Kx * inv(Kx + σnoise^2 * I)
-jim(Hw')
+jim(Hw; title="Wiener filter", color=:cividis)
 
 
 # Denoise via Wiener filter (MMSE *linear* method)
 Xw = Hw * Ytest
 nrmse = (Xh) -> round(norm(Xh - Xtest) / norm(Xtest) * 100, digits=2)
 @show nrmse(Ytest), nrmse(Xw)
-plot(Xw[:,1:4])
-plot!(Xtest[:,1:4])
+colors = [:blue, :red, :magenta, :green]
+plot(ylabel="signal value", title="Wiener filtering examples")
+for i in 1:4
+    plot!(Xw[:,i], color=colors[i])
+    plot!(Xtest[:,i], color=colors[i], line=:dash)
+    scatter!(Ytest[:,i], color=colors[i], marker=:star)
+end
 
 #
 prompt()
@@ -135,110 +138,117 @@ prompt()
 # Verify that marginal distribution is Gaussian
 histogram(Xtrain[:], label = "Xtrain hist")
 
+#
+prompt()
+
+
 #=
 ## Simple NN
 
-Examine a "NN" that is a single fully connected affine layer  
+Examine a "NN" that is a single fully connected affine layer
 (It should perform same as Wiener filter.)
 
 First try a basic affine NN model
 =#
-model1 = Chain(Dense(siz,siz))
-loss(model, x, y) = mse(model(x), y)
 
-iters = 2^12
-dataset = Base.Iterators.repeated((Ytrain, Xtrain), iters) # trick X,Y swap for denoising!
+if !@isdefined(state1)
+    model1 = Chain(Dense(siz,siz))
+    loss3(model, x, y) = mse(model(x), y)
 
-throw() # todo to here
+    iters = 2^12
+    dataset = Base.Iterators.repeated((Ytrain, Xtrain), iters) # trick X,Y swap for denoising!
 
-state1 = Flux.setup(loss, dataset, ADAM(Flux.params(model1)))
-Flux.train!(loss, dataset, ADAM(Flux.params(model1)))
+    state1 = Flux.setup(Adam(), model1)
+    Flux.train!(loss3, model1, dataset, state1)
+end;
 
-# +
-# compare training affine NN to wiener filter
-H1 = model1(Matrix(I, siz, siz)).data
-heatmap(H1, aspect_ratio=1, yflip=true, title="Learned filter for affine NN")
 
+# Compare training affine NN to wiener filter
+H1 = model1(Matrix(I, siz, siz))
+jim(H1; title="Learned filter for affine NN", colormap=:cividis)
+
+
+# Denoise test Data
 X1 = H1 * Ytest
-X1nn = model1(Ytest).data
+X1nn = model1(Ytest)
 @show nrmse(Ytest), nrmse(Xw), nrmse(X1), nrmse(X1nn)
-bias = model1(zeros(siz)).data
-@show minimum(bias), maximum(bias)
-plot!()
-# -
+bias = model1(zeros(siz))
+@show extrema(bias)
 
-# ### Examine a single hidden layer NN
 
-# +
-# create a basic NN model
-nhidden = siz*2 # neurons in hidden layer
-model2 = Chain(Dense(siz,nhidden,relu), Dense(nhidden,siz))
-loss(x, y) = mse(model2(x), y)
+#=
+## Examine a single hidden layer NN
 
-iters = 2^12
-dataset = Base.Iterators.repeated((Ytrain, Xtrain), iters) # trick X,Y swap for denoising!
+# Create a basic NN model
+=#
 
-evalcb = () -> @show(loss(Ytest,Xtest).data)
-cb = throttle(evalcb, 4)
+if !@isdefined(state2)
+    nhidden = 2siz # neurons in hidden layer
+    model2 = Chain(Dense(siz, nhidden, relu), Dense(nhidden, siz))
 
-Flux.train!(loss, dataset, ADAM(Flux.params(model2)), cb=cb)
-# -
+    iters = 2^12
+    dataset = Base.Iterators.repeated((Ytrain, Xtrain), iters) # trick X,Y swap for denoising!
 
-X2 = model2(Ytest).data
-@show nrmse(Ytest), nrmse(Xw), nrmse(X1), nrmse(X1nn), nrmse(X2)
+    state2 = Flux.setup(Adam(), model2)
+    Flux.train!(loss3, model2, dataset, state2)
+    X2 = model2(Ytest)
+end
+tmp = [Ytest, Xw, X1, X1nn, X2]
+@show nrmse.(tmp)
+
 
 # Examine joint distribution
 lag = 1
 tmp = circshift(Xtrain, (lag,))
-histogram2d(Xtrain[:], tmp[:])
+histogram2d(vec(Xtrain), vec(tmp))
 plot!(aspect_ratio=1, xlim=[-4,4], ylim=[-4,4])
 plot!(xlabel=L"x[n]", ylabel=latexstring("x[n-$lag \\ mod \\ N]"))
 plot!(title="Joint histogram of neighbors")
 
+#
+prompt()
 
 
-# ### Experiments below here - work in progress  
-# [https://github.com/FluxML/model-zoo/blob/master/vision/mnist/conv.jl]
+#=
+## WIP
+Experiments below here - work in progress
+[https://github.com/FluxML/model-zoo/blob/master/vision/mnist/conv.jl]
+=#
+if !@isdefined(model3)
 
-# +
-model3 = Chain(
-  Conv((2,2), 1=>16, relu),
-  x -> maxpool(x, (2,2)),
-  Conv((2,2), 16=>8, relu),
-  x -> maxpool(x, (2,2)),
-  x -> reshape(x, :, size(x, 4)))
-#  Dense(288, 10), softmax)
+    model3 = SkipConnection( # ResNet style: learn residual
+        Chain(
+            Conv((3,), 1 => 16, relu; pad = SamePad()),
+        ##  x -> maxpool(x, (2,2)),
+            Conv((3,), 16 => 8, relu; pad = SamePad()),
+            Conv((1,), 8 => 1, relu),
+        ##  x -> reshape(x, :, size(x, 4)),
+        ),
+        +,
+    )
 
-tmp = Xtrain[:,1:40];
-#model3(tmp)
-# -
+    shaper(X) = reshape(X, siz, 1, :) # (siz, channels, batch)
+    mymodel3(X) = model3(shaper(X))[:, 1, :]
+    @assert size(mymodel3(Xtrain)) == size(Xtrain)
 
-tmp = Conv((3,1), 1=>1, relu)
-model3 = Chain(tmp)
-#model3 = Chain(Dense(siz,nhidden,relu))
-tmp = reshape(Xtrain[:,1], siz, 1);
-#model3(tmp)
+    nouter = 2^2
+    ninner = 2^3
+    ## trick X,Y swap for denoising!
+    dataset = Base.Iterators.repeated((shaper(Ytrain), shaper(Xtrain)), ninner)
+    for io in 1:nouter
+        state3 = Flux.setup(Adam(), model3)
+        Flux.train!(loss3, model3, dataset, state3)
+        X3train = mymodel3(Ytrain)
+        @show io, loss3(model3, shaper(Xtrain), shaper(Ytrain))
+        ## todo: validation data too
+    end
+end
 
-tmp = Conv((3,3), 1=>1, relu)
-model3 = Chain(tmp)
-#model3 = Chain(Dense(siz,nhidden,relu))
-tmp = reshape(Xtrain[:,1], siz, 1)
-tmp = Xtrain[:,1:4];
-#model3(tmp)
 
-# +
-# create a basic NN model
-nhidden = siz*2 # neurons in hidden layer
-model3 = Chain(Dense(siz,nhidden,relu), Dense(nhidden,siz))
-loss(x, y) = mse(model2(x), y)
+X3test = mymodel3(Ytest)
+tmp = [Ytest, Xw, X1, X1nn, X2, X3test]
+@show nrmse.(tmp) # todo: no improvement!?
 
-iters = 2^12
-dataset = Base.Iterators.repeated((Ytrain, Xtrain), iters) # trick X,Y swap for denoising!
-
-evalcb = () -> @show(loss(Ytest,Xtest).data)
-cb = throttle(evalcb, 4);
-
-#Flux.train!(loss, dataset, ADAM(Flux.params(model2)), cb=cb)
 
 
 # ### Reproducibility
